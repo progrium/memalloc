7babf6be0527b1e1b8f6b630d70bfb6f7fe58db8
root <root@packt>
2014-05-05 20:58:18 +0000

    use memory

--- mem.go
+++ mem.go
@@ -10,13 +10,20 @@ import (
 
 func main() {
 	var megabytes int
-	if len(os.Args) < 1 {
+	if len(os.Args) < 2 {
 		megabytes = 1
 	} else {
 		megabytes, _ = strconv.Atoi(os.Args[1])
 	}
 	mem := make([]byte, 1024 * 1024 * megabytes)
-	fmt.Println("Megabytes allocated:", megabytes, "(", len(mem),")")
+	fmt.Println("Allocated:", megabytes, "mb (", len(mem),"b )")
+	if len(os.Args) == 3 {
+		megabytes, _ := strconv.Atoi(os.Args[2])
+		for i := 0; i < (1024 * 1024 * megabytes); i++ {
+			mem[i] = byte(255)
+		}
+		fmt.Println("Used:", megabytes, "mb")
+	}
 	sc := make(chan os.Signal, 2)
 	signal.Notify(sc, syscall.SIGTERM, syscall.SIGINT)
 	<-sc
